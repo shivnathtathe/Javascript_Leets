c3d663ded8e2e507d986fbd53332da8f0823dfc0
Shivnath Tathe <76526542+shivnathtathe@users.noreply.github.com>
2024-03-11 16:03:11 +0530

    Time: 31 ms (80.87%), Space: 16.6 MB (64.23%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,7 +1,6 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         new_list = s.split(" ")
-        str_ = ""
         new_list2 = []
         for x in range(k):
             new_list2.append(new_list[x])
