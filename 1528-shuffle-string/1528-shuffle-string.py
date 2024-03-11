ec1dd3bf5f8c427775aaf605fb7f51cfdf678c12
Shivnath Tathe <76526542+shivnathtathe@users.noreply.github.com>
2024-03-11 16:30:25 +0530

    Time: 59 ms (24.53%), Space: 16.5 MB (56.77%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,9 +1,15 @@
+from typing import List
+
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        str_ =""
+        # Create a dictionary to map indices to characters
+        char_map = {}
+        for index, char in zip(indices, s):
+            char_map[index] = char
         
-        for num in range(len(s)):
-            for j in indices:
-                if num == j:
-                    str_ += s[indices.index(j)]
-        return str_
+        # Reconstruct the string using the dictionary
+        result = ""
+        for i in range(len(indices)):
+            result += char_map[i]
+        
+        return result
